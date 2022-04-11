776f48964e31a4b6acc4e74f12dcc27ccd70e342
bhaskarmenugula <bhaskarmenugula@gmail.com>
2022-04-11 21:03:12 +0530

    Bhaskar added CR1A in featureA bugfix

--- welcome.py
+++ welcome.py
@@ -1,6 +1,6 @@
 print("Hello world")
 print("Bhaskar dev feature A")
-print("Bhaskar dev feature A - bugfix")
+print("Bhaskar dev feature A - CR1A bugfix")
 print("2021mt93695 added feature B")
 print("Bhaskar merge dev to prod.")
 print("bhaskar gitignore demo")
